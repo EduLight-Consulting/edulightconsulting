2cf04c2335a37dc2c8aa3d2c9a6f0a728489ff19
AbidemiT <abidemis01@gmail.com>
2018-09-17 23:49:53 +0100

    Redesigned Landing Page

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,10 @@
     </label>
   </div>
   <input type="checkbox" id="nav-check">
-        <div class="nav-links" ><router-link class="link" to="/about">About</router-link>
-        <router-link class="link" to="/profile">Profiles</router-link>
-        <router-link class="link" to="/staffs">Staffs</router-link>
-        <router-link class="link" to="/testimonials">Testimonials</router-link></div>
+        <div class="nav-links" ><router-link class="link" to="/about">ABOUT</router-link>
+        <router-link class="link" to="/profile">PROFILES</router-link>
+        <router-link class="link" to="/staffs">STAFFS</router-link>
+        <router-link class="link" to="/testimonials">TESTIMONIALS</router-link></div>
       </nav>
    </header>
    <transition name="router-anim" enter-active-class="animated bounceIn" leave-active-class="animated bounceOut">
@@ -43,12 +43,18 @@ export default {
   margin: 0;
   color: #fff;
 }
+.nav-btn {
+  display: none;
+}
+#nav-check {
+  display: none;
+}
 #header{
   padding: 20px;
-  background-color: #e65454; 
+  background-image: url("./assets/astar.jpeg"); 
   display: grid;
   border-radius: 2px;
-  grid-template-columns: 150px 1fr 320px; 
+  grid-template-columns: 120px 1fr 420px; 
 }
 #app{
   position: relative;
@@ -63,10 +69,73 @@ nav{
 .link{
     padding: 10px;
     margin-right: 5px;
-    background-color: rgba(80, 159, 224, 0.933);
+    background-color: transparent;
     color: #fff;
     text-decoration: none;
     border-radius: 5px;
-    box-shadow: 0 0 2px 0px rgb(8, 7, 7); 
+    /* box-shadow: 0 0 2px 0px rgb(8, 7, 7);  */
+}
+.link:hover{
+    padding: 10px;
+    margin-right: 5px;
+    background-color: transparent;
+    color: #38ce;
+    text-decoration: none;
+    border-radius: 5px;
+    /* box-shadow: 0 0 2px 0px rgb(8, 7, 7);  */
+}
+@media (max-width:500px) {
+  #header{
+    padding: 30px;
+    background-color: #2c3e50; 
+    display: grid;
+    color: aliceblue;
+    grid-template-columns: 100px 1fr 200px; 
+}
+  .nav-btn {
+    display: inline-block;
+    position: absolute;
+    right: 0px;
+    top: 0px;
+  }
+  .nav-btn > label {
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+    padding-top: 30px;
+  }
+ .nav-btn > label:hover {
+    cursor: pointer;
+  }
+.nav-links {
+    position: absolute;
+    display: block;
+    width: 100%;
+    background-color: transparent;
+    height: 0px;
+    transition: all 0.3s ease-in;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    top: 91px;
+    left: 0px;
+  }
+  .nav-links > router-link {
+    display: block;
+    width: 100%;
+  }
+  .nav-btn > label > span {
+    display: block;
+    width: 25px;
+    height: 10px;
+    border-top: 2px solid #eee;
+  }
+  #nav-check:not(:checked) + .nav-links {
+    height: 0px;
+  }
+  #nav-check:checked + .nav-links {
+    margin-top: -3px;
+    height: calc(18vh - 91px);
+    overflow-y: hidden;
+  }
 }
 </style>
--- src/components/edulight.vue
+++ src/components/edulight.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="edulight">
     <main>
-      <div>WELCOME TO EDULIGHT CONSULTING
-        <button></button>
+      <div class="land"><h1>WELCOME TO EDULIGHT CONSULTING</h1>
       </div>
+        <div class="btn-element">
+          <button class="btn">About</button>
+          <button class="btn">Testimonials</button>
+        </div>
       
       
     </main>
@@ -23,9 +26,23 @@ export default {
   main{
     display: grid;
     grid-template-columns: 1fr;
-    /* height:87.6vh; */
-    background-image: url("../assets/office.jpeg");
-    
-  filter: opacity(80%);
+    grid-template-rows: 1fr 1fr;
+    height:87.6vh;
+    background-image: url("../assets/astar.jpeg");
+  }
+  .land{
+    margin-top: 200px;
+    font-size:23px;
+  }
+  .btn{
+    margin: 10px;
+    padding:10px;
+    border-radius: 7px;
+    outline:none;
+    cursor: pointer;
+    background-color: rgb(209, 51, 51);
+  }
+  .btn-element{
+    margin-bottom: 80px;
   }
 </style>
